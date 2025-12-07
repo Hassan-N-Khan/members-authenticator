f3873a9b5ff0aa317f5cf47484f1f832069c6904
Hassan Khan <hassannadeemkhan@gmail.com>
2025-12-07 15:21:56 -0500

    fixed the logout button not working

--- server/app.js
+++ server/app.js
@@ -104,9 +104,14 @@ app.post("/log-in", async (req, res, next) => {
 
 app.get("/log-out", (req, res, next) => {
   req.logout((err) => {
-    if (err) {
-      return next(err);
-    }
+    if (err) return next(err);
+
+    req.session.destroy((err) => {
+      if (err) return next(err);
+
+      res.clearCookie("connect.sid");
+      res.json({ success: true });
+    });
   });
 });
 
